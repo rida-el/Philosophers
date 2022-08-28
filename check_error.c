22cd12186be585ab210ff7eb1b3a7b7fb44479b2
rida-el <rida.el99@gmail.com>
2022-08-28 15:04:00 +0100

    check numbers and args

--- check_error.c
+++ check_error.c
@@ -19,37 +19,36 @@ int ft_chaeck_args(char *str)
     i = 0;
     while (str[i])
     {
-        if (str[i] < '0' || str[i] > '9' )
+        if (str[i] < '0' || str[i] > '9')
             return (0);
         i++;
     }
     return (1);
-    
 }
 
-int ft_check_numbers(int argc, char **argv) 
+int ft_check_numbers(int argc, char **argv)
 {
-    if(!ft_check_args(argv[1]) || !ft_check_args(argv[2]) || !ft_check_args(argv[3]) || !ft_check_args(argv[4]))
-        return(prinf("error"),0);
-    if(argc == 6)
+    if (!ft_check_args(argv[1]) || !ft_check_args(argv[2]) || !ft_check_args(argv[3]) || !ft_check_args(argv[4]))
+        return (prinf("error"), 0);
+    if (argc == 6)
         if (!ft_check_args(argv[5]))
-            return(prinf("error"),0);
-    return(1);
+            return (prinf("error"), 0);
+    return (1);
 }
 
 int ft_check_error(t_utils utils, int argc, char **argv)
 {
-    if(!ft_check_numbers(argc, argv))
-        return(prinf("error"),0);
+    if (!ft_check_numbers(argc, argv))
+        return (prinf("error\n"), 0);
+    if (argc == 6 && utils.nbr_of_times_each_philo_must_eat <= 0)
+        return (prinf("error\n"), 0);
     if (utils.nbr_of_philo < 1)
-		return (printf("404"), 0);
-	else if (argc == 6)
-	{
-		if (utils.time_to_eat < 0)
-			return (printf("404"), 0);
-	}
-	else if (utils.time_to_die < 60 || utils.time_to_eat < 60 || utils.time_to_sleep < 60)
-		return (printf("error"), 0);
-	return (1);
-    
+        return (prinf("error num fo philo\n"), 0);
+    if (utils.time_to_die < 60)
+        return (prinf("error time to die\n"), 0);
+    if (utils.time_to_eat < 60)
+        return (prinf("error time to eat\n"), 0);
+    if (utils.time_to_sleep < 60)
+        return (prinf("error time to sleep\n"), 0);
+    return (1);
 }
