5da42fb8cfa42790af7d12971cf405b886cce8ca
rida el mazary <rida.el99@gmail.com>
2022-08-30 17:00:41 +0100

    fix and update in philo.c

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: rel-maza <rel-maza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 22:34:34 by rel-maza          #+#    #+#             */
-/*   Updated: 2022/08/29 15:48:30 by rel-maza         ###   ########.fr       */
+/*   Updated: 2022/08/30 14:19:07 by rel-maza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int main(int argc, char **argv)
 	i = 0;
     if (argc != 5 && argc != 6)
         return (printf("error\n") , 1);
-    ft_insert_args(utils, argc, argv);
-    if (!ft_check_error(utils, argv, argc))
+    ft_insert_args(&utils, argc, argv);
+    if (!ft_check_error(utils, argc, argv))
         return (0);
     philo = malloc(sizeof(t_philo) * utils.nbr_of_philo);
     utils.fork = malloc(sizeof(pthread_mutex_t) * utils.nbr_of_philo);
